ab4d3480ec291a86d49f763a4fde3e5a310145f5
ShreyasZope <shreyaszope123@gmail.com>
2024-12-24 09:19:49 +0530

    tittle change

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,7 +16,7 @@ function Home(){
         <React.Fragment>
         <Helmet>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <title>My Title</title>
+        <title>ATG WORLD</title>
         </Helmet>
         <div className='home-page'>
         <div className='search-bar'>
--- src/Components/signin.js
+++ src/Components/signin.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Signin(){
     return(
         <div> 
-            SIGNIN
+            
             </div>
     )
 }
